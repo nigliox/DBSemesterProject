bf98adab3b10e6ef0746ed547427d541cc53dcd7
Nigliox <fm@ct-s.ch>
2019-03-09 21:54:34 +0100

    views added v_usages per Location and Pod ToDo other views

--- DML/DML_Views.sql
+++ DML/DML_Views.sql
@@ -1,5 +1,6 @@
 use inventarisierungslösung;
 
+# view for getting informations about the usage in percent perLocation
 CREATE VIEW v_UsagePerLocation as
 
 SELECT idLocation,locationName,strasse,hausnummer,plz,ort,land,idDevice,idDevice_Typ,beschreibung, count(idDevice) as belegtePorts, anzahlPorts, (count(idDevice)/anzahlPorts * 100) as usageInPercent
@@ -11,3 +12,19 @@ INNER JOIN location l ON d.fk_idLocation = l.idLocation
 INNER JOIN adresse a ON a.idAdresse = l.fk_idAdresse
 GROUP BY idDevice;
 
+
+# view for getting informations about the usage in percent perPod
+CREATE VIEW v_usagePerPod as
+SELECT idPOD,idLocation,locationName,strasse,hausnummer,plz,ort,land,idDevice,idDevice_Typ,beschreibung,count(idDevice) as belegtePorts, sum(DISTINCT anzahlPorts) as anzahlPorts, (count(idDevice)/sum(DISTINCT anzahlPorts) * 100) as usageInPercent
+
+from device d 
+INNER JOIN device_typ dt ON d.fk_idDeviceType = dt.idDevice_Typ	
+INNER JOIN netzwerkinterface nt ON nt.fk_idDevice = d.idDevice
+INNER JOIN location l ON d.fk_idLocation = l.idLocation
+INNER JOIN adresse a ON a.idAdresse = l.fk_idAdresse
+INNER JOIN pod p ON p.idPOD = l.fk_idPOD
+GROUP BY idPOD;
+
+# view for getting information about free ports and speed options on networkinterfaces
+
+SELECT locationName, Hostname, Portnu
